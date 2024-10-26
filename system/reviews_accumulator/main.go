b9c4f4181f98c204b8c1a652026f5f8d29edd660
rafab15 <rafaelberenguel15@gmail.com>
2024-10-26 20:15:14 -0300

    Adding graceful shutdown for reviews accumulator

--- system/reviews_accumulator/main.go
+++ system/reviews_accumulator/main.go
@@ -4,6 +4,9 @@ import (
 	u "distribuidos-tp/internal/utils"
 	l "distribuidos-tp/system/reviews_accumulator/logic"
 	m "distribuidos-tp/system/reviews_accumulator/middleware"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/op/go-logging"
 )
@@ -15,6 +18,11 @@ const (
 var log = logging.MustGetLogger("log")
 
 func main() {
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, syscall.SIGTERM, syscall.SIGINT)
+
+	doneChannel := make(chan bool)
+
 	accumulatorsAmount, err := u.GetEnvInt(MappersAmountEnvironmentVariableName)
 	if err != nil {
 		log.Errorf("Failed to get environment variable: %v", err)
@@ -28,5 +36,13 @@ func main() {
 	}
 
 	reviewsAccumulator := l.NewReviewsAccumulator(middleware.ReceiveReviews, middleware.SendAccumulatedReviews, middleware.SendEof)
-	reviewsAccumulator.Run(accumulatorsAmount)
+
+	go u.HandleGracefulShutdown(middleware, signalChannel, doneChannel)
+
+	go func() {
+		reviewsAccumulator.Run(accumulatorsAmount)
+		doneChannel <- true
+	}()
+
+	<-doneChannel
 }
