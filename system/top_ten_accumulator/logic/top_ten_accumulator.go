3c3968ddc02f73cea0ce5be7bc781c2b7cf1a531
franco-jyq <fgazzola@fi.uba.ar>
2024-11-09 16:08:48 -0300

    feat: Persistence layer in Top Ten node.

--- system/top_ten_accumulator/logic/top_ten_accumulator.go
+++ system/top_ten_accumulator/logic/top_ten_accumulator.go
@@ -2,6 +2,8 @@ package top_ten_accumulator
 
 import (
 	df "distribuidos-tp/internal/system_protocol/decade_filter"
+	rp "distribuidos-tp/system/top_ten_accumulator/persistence"
+	"fmt"
 
 	"github.com/op/go-logging"
 )
@@ -11,20 +13,18 @@ var log = logging.MustGetLogger("log")
 type TopTenAccumulator struct {
 	ReceiveMsg func() (int, []*df.GameYearAndAvgPtf, bool, error)
 	SendMsg    func(int, []*df.GameYearAndAvgPtf) error
+	Repository *rp.Repository
 }
 
 func NewTopTenAccumulator(receiveMsg func() (int, []*df.GameYearAndAvgPtf, bool, error), sendMsg func(int, []*df.GameYearAndAvgPtf) error) *TopTenAccumulator {
 	return &TopTenAccumulator{
 		ReceiveMsg: receiveMsg,
 		SendMsg:    sendMsg,
+		Repository: rp.NewRepository("top_ten_accumulator"),
 	}
 }
 
 func (t *TopTenAccumulator) Run(decadeFilterAmount int, fileName string) {
-
-	// remainingEOFs := decadeFilterAmount
-	topTenGames := make(map[int][]*df.GameYearAndAvgPtf)
-	remainingEOFs := make(map[int]int)
 
 	for {
 
@@ -34,60 +34,35 @@ func (t *TopTenAccumulator) Run(decadeFilterAmount int, fileName string) {
 			return
 		}
 
-		clientTopTenGames, exists := topTenGames[clientID]
-		if !exists {
-			clientTopTenGames = []*df.GameYearAndAvgPtf{}
-			topTenGames[clientID] = clientTopTenGames
+		clientTopTenGames, err := t.Repository.Load(clientID)
+		if err != nil {
+			log.Errorf("failed to load client %d top ten games: %v", clientID, err)
+			continue
 		}
 
 		log.Infof("Received decade games")
 
 		if eof {
+			remainingEOFs, err := t.Repository.PersistAndUpdateEof(clientID, decadeFilterAmount)
+			if err != nil {
+				fmt.Printf("failed to persist and update eof: %v\n", err)
+				continue
+			}
 
-			// Si no existe inicializo el contador de EOFs restantes
-			if _, ok := remainingEOFs[clientID]; !ok {
-				remainingEOFs[clientID] = decadeFilterAmount - 1
-			} else {
-				remainingEOFs[clientID]--
+			if remainingEOFs <= 0 {
+				log.Infof("Received all EOFs of client %d, sending final top ten games", clientID)
 
-				if remainingEOFs[clientID] <= 0 {
-					log.Infof("Received all EOFs of client %d, sending final top ten games", clientID)
-					/*finalTopTenGames, err := df.UploadTopTenAvgPlaytimeForeverFromFile("top_ten_games")
-					if err != nil {
-						log.Errorf("error uploading top ten games from file: %v", err)
-						return
-					}*/
-					err = t.SendMsg(clientID, clientTopTenGames)
-					if err != nil {
-						log.Errorf("failed to send metrics: %v", err)
-						return
-					}
-					delete(topTenGames, clientID)
-					delete(remainingEOFs, clientID)
+				err = t.SendMsg(clientID, clientTopTenGames)
+				if err != nil {
+					log.Errorf("failed to send metrics: %v", err)
+					return
 				}
 			}
 		}
 
 		clientTopTenGames = df.TopTenAvgPlaytimeForever(append(clientTopTenGames, decadeGames...))
-		topTenGames[clientID] = clientTopTenGames
+		t.Repository.Persist(clientID, clientTopTenGames)
 		log.Infof("Updated top ten games for client %d", clientID)
-
-		//topTenGames := df.TopTenAvgPlaytimeForever(decadeGames)
-
-		// Upload the actual top ten games from the file and update the top ten games
-		/*actualTopTenGames, err := df.UploadTopTenAvgPlaytimeForeverFromFile(fileName)
-		if err != nil {
-			log.Errorf("Error uploading top ten games from file: %v", err)
-			return
-		}
-		updatedTopTenGames := df.TopTenAvgPlaytimeForever(append(topTenGames, actualTopTenGames...))
-
-		// Save the updated top ten games to the file
-		err = df.SaveTopTenAvgPlaytimeForeverToFile(updatedTopTenGames, "top_ten_games")
-		if err != nil {
-			log.Errorf("error saving top ten games to file: %v", err)
-			return
-		}*/
 
 	}
 }
