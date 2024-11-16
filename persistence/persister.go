3b0fc8e3365b47335434f5e72de9332ca3a12e95
rafab15 <rafaelberenguel15@gmail.com>
2024-11-16 15:07:39 -0300

    Adding the waitgroupd to the persister so it blocks when reading or writing a file

--- persistence/persister.go
+++ persistence/persister.go
@@ -4,7 +4,9 @@ import (
 	u "distribuidos-tp/internal/utils"
 	"encoding/binary"
 	"errors"
+	"github.com/op/go-logging"
 	"os"
+	"sync"
 )
 
 const (
@@ -16,10 +18,12 @@ type Persister[T any] struct {
 	fileName    string
 	serialize   func(*T) ([]byte, error)
 	deserialize func([]byte) (*T, error)
+	wg          *sync.WaitGroup
+	logger      *logging.Logger
 }
 
-func NewPersister[T any](fileName string, serialize func(*T) ([]byte, error), deserialize func([]byte) (*T, error)) *Persister[T] {
-	return &Persister[T]{fileName, serialize, deserialize}
+func NewPersister[T any](fileName string, serialize func(*T) ([]byte, error), deserialize func([]byte) (*T, error), wg *sync.WaitGroup, logger *logging.Logger) *Persister[T] {
+	return &Persister[T]{fileName, serialize, deserialize, wg, logger}
 }
 
 func (p *Persister[T]) Save(data *T) error {
@@ -27,6 +31,8 @@ func (p *Persister[T]) Save(data *T) error {
 	if err != nil {
 		return err
 	}
+
+	p.wg.Add(1)
 
 	oldFileName := p.fileName + CopySuffix
 	err = os.Rename(p.fileName, oldFileName)
@@ -38,7 +44,14 @@ func (p *Persister[T]) Save(data *T) error {
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			p.logger.Errorf("Error closing file: %v", err)
+		}
+		p.wg.Done()
+	}()
 
 	lengthBytes := make([]byte, FileLengthSize)
 	binary.BigEndian.PutUint64(lengthBytes, uint64(len(serializedData)))
@@ -57,11 +70,20 @@ func (p *Persister[T]) Save(data *T) error {
 }
 
 func (p *Persister[T]) Load() (*T, error) {
+	p.wg.Add(1)
+
 	file, err := os.OpenFile(p.fileName, os.O_RDONLY|os.O_CREATE, 0644)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			p.logger.Errorf("Error closing file: %v", err)
+		}
+		p.wg.Done()
+	}()
 
 	fileLengthBytes, err := u.ReadExact(file, FileLengthSize)
 	if err != nil {
